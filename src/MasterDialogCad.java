99a1f454f8647498d87537547fdb7868d9254812
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2019-10-04 20:34:34 -0300

    master dialog cad e utils

---
+++ src/MasterDialogCad.java
@@ -0,0 +1,235 @@
+import java.awt.Container;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
+
+
+@SuppressWarnings("serial")
+public class MasterDialogCad extends JInternalFrame {
+	// Botoes
+	protected JToolBar toolbar;
+	protected JButton btnAdd;
+	protected JButton btnSearch;	
+	protected JButton btnDelete;
+	protected JButton btnSave;	
+	protected JButton btnCancel;
+	protected Container childContainer;
+	protected Utils utils;
+
+	protected boolean isInserting;
+	protected AbstractAction actDel = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			try {
+				setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));			
+				if (actionDelete()) {
+					setFieldsEnabled(false);
+					clean();
+				}
+			}finally {
+				setCursor(Cursor.getDefaultCursor());
+			}
+		}
+	};
+
+	protected AbstractAction actAdd = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(actionAdd()) {
+				isInserting = true;
+				clean();
+				fillFields();
+				setFieldsEnabled(true);				
+			}
+		}
+	};
+
+	protected AbstractAction actSearch = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			actionSearch();
+		}
+	};
+
+	protected WindowAdapter eventWindowSearchClosed = new WindowAdapter() {
+		
+		public void windowClosed(WindowEvent e) {
+			
+			if(afterSearch()) {
+				clean();
+				fillFields();
+				isInserting = false;
+				setFieldsEnabled(true);
+			}
+			
+		}		
+			
+	};
+
+	protected AbstractAction actSave = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+				if(actionSave()) {
+					if(isInserting) {
+						btnCancel.doClick();
+						clean();
+					}
+				}
+			} finally {
+				setCursor(Cursor.getDefaultCursor());
+			}
+
+		}
+	};
+
+	protected AbstractAction actCancel = new AbstractAction() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			if(actionCancel()) {
+				if(!isInserting) {
+					fillFields();
+				}else {
+					isInserting = false;
+					setFieldsEnabled(false);
+					clean();
+				}
+
+			}
+		}
+	};
+
+	public static void main(String Args[]) {
+		new MasterDialogCad();
+	}
+
+	public MasterDialogCad() {
+
+		utils = new Utils();
+		Componnents();
+		subComponents();
+		setFieldsEnabled(false);
+
+	}
+
+	protected void setFieldsEnabled(boolean enabled) {
+		utils.setSubComponentsEnabled(childContainer, enabled);
+	}
+
+	protected void subComponents() {
+
+	}
+
+	protected boolean actionDelete() {
+		return false;
+	}
+
+	protected boolean actionAdd() {
+		return true;
+	}
+
+	protected void actionSearch() {
+	}
+
+	protected boolean actionSave() {
+		return false;
+	}
+
+	protected boolean actionCancel() {
+		return false;
+	}
+
+	protected boolean afterSearch() {
+		return false;		
+	}
+
+	protected void fillFields() {
+
+	}
+
+	protected void clean() {
+		utils.cleanSubComponents(childContainer);
+	}
+
+	private void Componnents() {
+
+		toolbar = new JToolBar();
+		toolbar.setAlignmentX(LEFT_ALIGNMENT);
+		toolbar.setAlignmentY(TOP_ALIGNMENT);
+
+		//Botao Search
+		btnSearch = new JButton("Buscar", new ImageIcon(System.getProperty("user.dir") + "\\images\\22x22\\localizar.png"));
+		btnSearch.addActionListener(actSearch);
+		btnSearch.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSearch.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnSearch.setBounds(10, 10, 120, 35);
+		//getContentPane().add(btnSearch);	
+
+
+		// Botao Add
+		btnAdd = new JButton("Adicionar", new ImageIcon(System.getProperty("user.dir") + "\\images\\22x22\\adicionar.png"));
+		btnAdd.addActionListener(actAdd);
+		btnAdd.setHorizontalAlignment(SwingConstants.LEFT);
+		btnAdd.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnAdd.setBounds(130, 10, 120, 35);
+		//getContentPane().add(btnAdd);
+
+		// Botao Delete
+		btnDelete = new JButton("Remover", new ImageIcon(System.getProperty("user.dir") + "\\images\\22x22\\remover.png"));
+		btnDelete.addActionListener(actDel);
+		btnDelete.setHorizontalAlignment(SwingConstants.LEFT);
+		btnDelete.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnDelete.setBounds(250, 10, 120, 35);
+		//getContentPane().add(btnDelete);
+
+		// Botao Save
+		btnSave = new JButton("Salvar", new ImageIcon(System.getProperty("user.dir") + "\\images\\22x22\\salvar.png"));
+		btnSave.addActionListener(actSave);
+		btnSave.setHorizontalAlignment(SwingConstants.LEFT);
+		btnSave.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnSave.setBounds(370, 10, 120, 35);
+		//getContentPane().add(btnSave);
+
+		// Botao Cancel
+		btnCancel = new JButton("Cancelar", new ImageIcon(System.getProperty("user.dir") + "\\images\\22x22\\cancelar.png"));
+		btnCancel.addActionListener(actCancel);		
+		btnCancel.setHorizontalAlignment(SwingConstants.LEFT);
+		btnCancel.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnCancel.setBounds(490, 10, 120, 35);
+		//getContentPane().add(btnCancel);
+		toolbar.add(btnSearch);
+		toolbar.addSeparator();;
+		toolbar.add(btnAdd);
+		toolbar.addSeparator();
+		toolbar.add(btnDelete);
+		toolbar.addSeparator();
+		toolbar.add(btnSave);
+		toolbar.addSeparator();
+		toolbar.add(btnCancel);
+		
+		toolbar.setBounds(10, 10, 600, 40);		
+		getContentPane().add(toolbar);
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/Utils.java
@@ -0,0 +1,168 @@
+import java.awt.Component;
+import java.awt.Container;
+import java.text.Collator;
+import java.util.Locale;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JViewport;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.text.JTextComponent;
+
+public class Utils {
+
+	//Descontinuado - Foi melhorado para usar os objetos Class
+	// Insira as classes dos componentes para serem desabilitados
+	/*
+	private String targets[] = {
+			"javax.swing.JTextField",
+			"javax.swing.JComboBox",
+			"javax.swing.JFormattedTextField",
+			"javax.swing.JTextArea"
+	};
+	 */
+
+	// Insira as classes dos componentes para serem desabilitados
+	// Note que para Classes como a JComboBox, deve ser implementado uma forma
+	// especifica para limpar o componente na função cleanFields(), ja os componentes
+	// que extendem JTextComponent como JtextArea e JTextField não é necessario implementar.
+	private Class<?> targetClasses[] = {
+			JTextField.class,
+			JFormattedTextField.class,
+			JComboBox.class,
+			JTextArea.class,
+			JScrollPane.class,
+			JTable.class,
+			JButton.class
+	};
+
+
+	public void cleanSubComponents(Container container) {
+		cleanComponents(container.getComponents());
+	}
+
+	public void cleanComponents(Component[] components) {
+		for(Component component : components) {		
+
+			if (component!=null) {
+				if (isInstanceOfSomeTarget(component)) {
+					if (JComboBox.class.isInstance(component)) {
+						((JComboBox<?>) component).setSelectedIndex(0);;	
+					}else if (JScrollPane.class.isInstance(component)) {
+						cleanComponents(getSubComponents(component));
+					}else if (JTable.class.isInstance(component)){
+						((DefaultTableModel) ((JTable) component).getModel()).setRowCount(0);
+					} else if (JButton.class.isInstance(component)) {
+						//Ignore;
+					} else {
+						((JTextComponent) component).setText("");
+					}
+				}
+			}
+
+		}
+	}
+
+	//Pega um container passado pela função getContentPane, traz todos componentes abaixo dele,
+	//e desabilita os que estao contidos em alvo e não sa nulos
+	public void setSubComponentsEnabled(Container container, Boolean enabled) {
+
+		setComponentsEnabled(container.getComponents(), enabled);			
+
+	}
+
+	public void setComponentsEnabled(Component[] components, Boolean enabled) {
+		for(Component component : components) {		
+
+			if (component!=null && isInstanceOfSomeTarget(component)/*Target(field.getClass().getName())*/) {	
+				if(JScrollPane.class.isInstance(component)) {
+					//Tratamento especifico para SrollPane, é passado todos componentes dentro do Scroll de forma recursiva
+					setComponentsEnabled( getSubComponents(component), enabled);			
+				}else {
+					if(component.getName()==null) {
+						component.setEnabled(enabled);
+					}
+				}				
+			}
+
+		}
+	}
+
+	private Component[] getSubComponents(Component component) {
+		return ((JViewport) ((JScrollPane) component).getComponent(0)).getComponents();
+	}
+
+
+	// Descontinuado - Use isInstanceOfSomeTarget(Component component) 
+	// Verifica se classe esta cadastrada como um alvo para ser desabilitada
+	/*private Boolean isTarget(String componentClass) {
+
+		for(String target : targets ) {
+			if(target.equals(componentClass)) {
+				return true;
+			}
+		}
+
+		return false;
+
+	}*/
+
+
+	private boolean isInstanceOfSomeTarget(Component component) {
+
+		for(Class<?> target : targetClasses) {
+			if(target.isInstance(component)) {
+				return true;								
+			}
+		}
+		return false;
+	}
+	
+	public boolean containsIgnoreCase(String str, String searchStr)     {
+	    if(str == null || searchStr == null) return false;
+
+	    final int length = searchStr.length();
+	    if (length == 0)
+	        return true;
+	    
+	    if (length > str.length())
+	    	return false;
+
+	    for (int i = str.length() - length; i >= 0; i--) {
+	        if (str.regionMatches(true, i, searchStr, 0, length))
+	            return true;
+	    }
+	    return false;
+	}
+
+	public boolean compareStrings(String strA, String StrB) {
+
+		Collator coll = Collator.getInstance (new Locale ("pt", "BR"));
+		coll.setStrength(Collator.PRIMARY);
+
+		return coll.compare(strA, StrB) == 0;
+
+	}
+	
+	public boolean tableContains(JTable table, int column, String str) {
+		DefaultTableModel model = ((DefaultTableModel) table.getModel());
+		
+		for(int i = 0; i < model.getRowCount(); i++) {
+			
+			if (model.getValueAt(i, column).toString().equals(str)) {
+				return true;
+			}
+			
+		}
+		
+		return false;		
+	}
+
+
+
+}
