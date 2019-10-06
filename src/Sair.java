fbd8c752603959dba3ea85e2ba35d1d113ccc37f
chaucoski <48536256+chaucoski@users.noreply.github.com>
2019-10-05 23:34:32 -0300

    Update Sair.java

--- src/Sair.java
+++ src/Sair.java
@@ -2,22 +2,23 @@
 import java.awt.Dimension;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 
 @SuppressWarnings("serial")
-public class Sair  extends JInternalFrame  {
-
+public class Sair extends JInternalFrame {
 
 	public Sair() {
-		
-		System.exit(EXIT_ON_CLOSE);
-		
+		if (JOptionPane.showConfirmDialog(null, "Deseja realmente sair do sistema?", "Aviso",
+				JOptionPane.YES_NO_OPTION) == JOptionPane.OK_OPTION) {
+			System.exit(EXIT_ON_CLOSE);
+		} else {
+		}
 	}
-	
+
 	public void setPosicao() {
-		
+
 		Dimension d = this.getDesktopPane().getSize();
-	    this.setLocation((d.width - this.getSize().width) / 2, (d.height - this.getSize().height) / 2);
-	
+		this.setLocation((d.width - this.getSize().width) / 2, (d.height - this.getSize().height) / 2);
+
 	}
-	
 }
