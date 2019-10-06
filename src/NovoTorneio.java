4794722cd835d677bc41b529c8d157fc49cb3bb1
chaucoski <48536256+chaucoski@users.noreply.github.com>
2019-10-05 23:27:34 -0300

    Update NovoTorneio.java

--- src/NovoTorneio.java
+++ src/NovoTorneio.java
@@ -3,19 +3,21 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 @SuppressWarnings("serial")
 public class NovoTorneio extends MasterDialogCad {
 
 	private JLabel LblJogo, CodTorneio, NomeTorneio, QtdTimes, Obs;
+	private JTextField txtCodTorneio, txtNomeTorneio, txtQtdTimes, txtObs;
 	private JComboBox<String> ComboJogo;
 	private JButton BtnCriar;
 
 
 	private void create() {
 
-		setSize(690, 538);
+		setSize(640, 285);
 		setTitle("Novo Torneio");
 		setLayout(null);
 		setResizable(false);
@@ -37,11 +39,11 @@ public class NovoTorneio extends MasterDialogCad {
 
 
 		CodTorneio = new JLabel("Código do torneio:");		
-		CodTorneio.setBounds(135, 20, 200, 100);
+		CodTorneio.setBounds(30, 20, 200, 100);
 		getContentPane().add(CodTorneio);
 
 		NomeTorneio = new JLabel("Nome do Torneio:");		
-		NomeTorneio.setBounds(10, 50, 200, 100);
+		NomeTorneio.setBounds(32, 50, 200, 100);
 		getContentPane().add(NomeTorneio);
 
 		QtdTimes = new JLabel("Quantidade de Times:");		
@@ -49,22 +51,38 @@ public class NovoTorneio extends MasterDialogCad {
 		getContentPane().add(QtdTimes);
 
 		LblJogo = new JLabel("Tipo de jogo:");		
-		LblJogo.setBounds(10, 110, 200, 100);
+		LblJogo.setBounds(58, 110, 200, 100);
 		getContentPane().add(LblJogo);
 
 		ComboJogo = new JComboBox<String>();
 		ComboJogo.addItem("--Selecione--");
-		ComboJogo.setBounds(8, 180, 130, 26);
+		ComboJogo.setBounds(150, 147, 150, 26);
 		getContentPane().add(ComboJogo);
 
-		Obs = new JLabel("Observação");
-		Obs.setBounds(10, 175, 200, 100);
+		Obs = new JLabel("Observação:");
+		Obs.setBounds(59, 140, 200, 100);
 		getContentPane().add(Obs);
 
 		BtnCriar = new JButton("Criar Torneio");
-		BtnCriar.setBounds(10, 320, 150, 26);
+		BtnCriar.setBounds(150, 210, 120, 36);
 		getContentPane().add(BtnCriar);
-
+		
+		txtCodTorneio = new JTextField();
+		txtCodTorneio.setBounds(150, 57, 150, 26);
+		getContentPane().add(txtCodTorneio);
+		
+		txtNomeTorneio = new JTextField();
+		txtNomeTorneio.setBounds(150, 87, 150, 26);
+		getContentPane().add(txtNomeTorneio);
+		
+		txtQtdTimes = new JTextField();
+		txtQtdTimes.setBounds(150, 117, 150, 26);
+		getContentPane().add(txtQtdTimes);
+		
+		txtObs = new JTextField();
+		txtObs.setBounds(150, 177, 150, 26);
+		getContentPane().add(txtObs);
+		
 		childContainer = getContentPane();
 
 
