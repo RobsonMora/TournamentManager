c1fe87769623daa2ece4112ca375fec4197c7fdb
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2019-10-09 17:25:00 -0300

    criado tabela de partidas e adicionado inserts

--- Script SQL.sql
+++ Script SQL.sql
@@ -72,3 +72,62 @@ CREATE TABLE public.torneio_times
 
 ALTER TABLE public.torneio_times
     OWNER to postgres;
+
+CREATE TABLE public.torneio_partidas
+(
+    id serial NOT NULL,
+    id_torneio integer NOT NULL,
+    id_time_1 integer NOT NULL,
+    id_time_2 integer NOT NULL,
+    pontos_1 integer,
+    pontos_2 integer,
+    fase integer NOT NULL,
+    CONSTRAINT torneio_partidas_pk PRIMARY KEY (id),
+    CONSTRAINT torneio_partidas_fk FOREIGN KEY (id_torneio)
+        REFERENCES public.torneios (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT torneio_partidas_time1_fk FOREIGN KEY (id_time_1)
+        REFERENCES public.times (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT torneio_partidas_time2_fk FOREIGN KEY (id_time_2)
+        REFERENCES public.times (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE public.torneio_partidas
+    OWNER to postgres;
+	
+	
+	
+/*  Inserts iniciais, é necessario ter as tabelas limpas  */
+	
+--insere categorias 
+INSERT INTO public.categorias(nome) VALUES ('FPS');
+INSERT INTO public.categorias(nome) VALUES ('Card Game');
+INSERT INTO public.categorias(nome) VALUES ('MOBA');
+
+--insere jogos
+INSERT INTO public.jogos(nome, id_categoria) VALUES ('Counter-Strike: Global Offensive', 1);
+INSERT INTO public.jogos(nome, id_categoria) VALUES ('Hearthstone: Heroes of Warcraft', 2);
+INSERT INTO public.jogos(nome, id_categoria) VALUES ('League of Legends', 3);
+
+--insere times
+INSERT INTO public.times(nome) VALUES ('Mibr');
+INSERT INTO public.times(nome) VALUES ('Disguised Toast');
+INSERT INTO public.times(nome) VALUES ('ChocoTaco');
+INSERT INTO public.times(nome) VALUES ('Shroud');
+
+--insere torneio
+INSERT INTO public.torneios(nome, inicio, fim, id_jogo)	VALUES ('torneio internacional', '2019-11-01', '2019-11-15', 2);
+
+--insere times torneios
+INSERT INTO public.torneio_times(id_torneio, id_time) VALUES (1, 1);
+INSERT INTO public.torneio_times(id_torneio, id_time) VALUES (1, 2);
+INSERT INTO public.torneio_times(id_torneio, id_time) VALUES (1, 3);
+INSERT INTO public.torneio_times(id_torneio, id_time) VALUES (1, 4);
+
+
+	
