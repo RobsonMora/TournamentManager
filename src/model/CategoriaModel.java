35de84504f233c5d8ac74dd08e08af3e65ca7d65
Vitor Pereira <vitorpbemp@gmail.com>
2019-10-09 21:41:02 -0300

    Add files via upload

---
+++ src/model/CategoriaModel.java
@@ -0,0 +1,38 @@
+package model;
+
+public class CategoriaModel {
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaModel() {
+		setId(0);
+		setNome("");
+	}
+	
+	public CategoriaModel(CategoriaModel categoria) {
+		setId(categoria.getId());
+		setNome(categoria.getNome());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public CategoriaModel setId(Integer id) {
+		this.id = id;
+		return this;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public CategoriaModel setNome(String nome) {
+		this.nome = nome;
+		return this;
+	}
+	
+	
+
+}
